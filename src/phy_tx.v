5c2561cdbe5f7cd2bc0c5c594cb56a9f539ff316
Jfecoren <jfecoren@gmail.com>
2021-10-22 00:46:45 -0400

    phy_tx module advance

--- src/phy_tx.v
+++ src/phy_tx.v
@@ -3,24 +3,33 @@
 `include "clk_gen.v"
 `include "byte_striping.v"
 `include "m32_8.v"
+`include "paralelo_serial.v"
 
 
-module PHY_TX(data_out_0, data_out_1, valid_out_0, valid_out_1, data_in, valid_in, reset, clk_32f);
-	output [7:0] data_out_0, data_out_1;
-	output valid_out_0, valid_out_1;
+module PHY_TX(data_out_0, data_out_1, data_in, valid_in, reset, clk_32f);
+	output data_out_0, data_out_1;
 	input [31:0] data_in;
 	input valid_in, reset, clk_32f;
 
 	// wires
 	wire clk_f, clk_2f, clk_4f;
 	wire [31:0] lane_0, lane_1;
+	wire [7:0] data_8b_0, data_8b_1;
+	wire valid_out_0, valid_out_1;
 	wire valid_0, valid_1, valid_32_0, valid_32_1;
+	
 	// Clock generator module
 	clk_gen clock_gen(clk_f, clk_2f, clk_4f, clk_32f, reset);
 	// Byte Striping module
 	BYTE_STRIPING bte_strp(lane_0, lane_1, valid_0, valid_1, data_in, valid_in, clk_2f, clk_f);
 	//32b to 8b demuxes
-	m32b_8 conv_0(data_out_0, valid_out_0, lane_0, valid_0, reset, clk_4f);
-	m32b_8 conv_1(data_out_1, valid_out_1, lane_1, valid_1, reset, clk_4f);
+	m32b_8 conv_0(data_8b_0, valid_out_0, lane_0, valid_0, reset, clk_4f);
+	m32b_8 conv_1(data_8b_1, valid_out_1, lane_1, valid_1, reset, clk_4f);
+	
+	// Parallel to Serial Module
+	paralelo_serial parial_0(clk_4f, clk_32f, data_8b_0, valid_out_0, data_out_0);
+	paralelo_serial parial_1(clk_4f, clk_32f, data_8b_1, valid_out_1, data_out_1);
+	
+	
 
 endmodule
--- test/phy_tx_testbench.v
+++ test/phy_tx_testbench.v
@@ -5,38 +5,32 @@
 
 
 module TX_TESTBENCH;
-	wire [7:0] data_out_0, data_out_1;
-	wire valid_out_0, valid_out_1;
+	wire data_out_0, data_out_1;
 	wire [31:0] data_in;
 	wire valid_in, reset;
-  wire clk_32f, clk_4f, clk_2f, clk_f;
+  	wire clk_32f, clk_4f, clk_2f, clk_f;
 
 	clk_gen clk_gen(/*AUTOINST*/
 									.clk_2f			(clk_2f),
 									.clk_32f		(clk_32f),
 									.reset			(reset));
 
-  PHY_TX tx0(/*AUTOINST*/
-							.data_out_0      (data_out_0),
-              .data_out_1     (data_out_1),
-              .valid_out_0    (valid_out_0),
-              .valid_out_1    (valid_out_1),
-              .data_in        (data_in),
-              .valid_in       (valid_in),
-              .reset          (reset),
-              .clk_32f        (clk_32f)
-              );
+  PHY_TX tx0(/*AUTOINST*/ .data_out_0      (data_out_0),
+						  .data_out_1     (data_out_1),
+						  .data_in        (data_in),
+						  .valid_in       (valid_in),
+						  .reset          (reset),
+						  .clk_32f        (clk_32f)
+						  );
 
 TX_TESTER mtester(/*AUTOINST*/
 						.data_out_0      (data_out_0),
-            .data_out_1     (data_out_1),
-            .valid_out_0    (valid_out_0),
-            .valid_out_1    (valid_out_1),
-            .data_in        (data_in),
-            .valid_in       (valid_in),
-            .reset          (reset),
-            .clk_32f        (clk_32f),
-            .clk_2f         (clk_2f)
-            );
+						.data_out_1     (data_out_1),
+						.data_in        (data_in),
+						.valid_in       (valid_in),
+						.reset          (reset),
+						.clk_32f        (clk_32f),
+						.clk_2f         (clk_2f)
+						);
 
 endmodule
