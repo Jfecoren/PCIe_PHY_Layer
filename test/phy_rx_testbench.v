3ff64c72cc5b1dd3a6cc0618aee64764fa47f175
locampo010 <luisdocampo@gmail.com>
2021-10-22 19:04:58 -0600

    Update phy_rx_testbench.v

--- test/phy_rx_testbench.v
+++ test/phy_rx_testbench.v
@@ -11,13 +11,29 @@ module RX_TESTBENCH;
     wire clk_32f, clk_4f, clk_2f, clk_f;
 
 	clk_gen clk_gen(/*AUTOINST*/
-    
-    );
+									.clk_2f			(clk_2f),
+									.clk_32f		(clk_32f),
+									.reset			(reset));
 
-    PHY_RX rx0(/*AUTOINST*/
-    );
+    PHY_RX rx0(/*AUTOINST*/ .data_out       (data_out),
+						    .valid_out      (valid_out),
+                            .data_in_0      (data_in_0),
+						    .data_in_1      (data_in_1),
+                            .valid_in_0     (valid_in_0)
+                            .valid_in_1     (valid_in_1)
+						    .reset          (reset),
+						    .clk_32f        (clk_32f)
+						    );
 
-    RX_TESTER mtester(/*AUTOINST*/
-    );
+    RX_TESTER mtester(/*AUTOINST*/  .data_out       (data_out),
+						            .valid_out      (valid_out),
+                                    .data_in_0      (data_in_0),
+						            .data_in_1      (data_in_1),
+                                    .valid_in_0     (valid_in_0)
+                                    .valid_in_1     (valid_in_1)
+						            .reset          (reset),
+						            .clk_32f        (clk_32f),
+                                    .clk_2f         (clk_2f)
+						            );
 
 endmodule
